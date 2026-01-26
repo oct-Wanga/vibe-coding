2c5868861bb59d0215a64e582bcc7b0c794ac3f7
jmwang <jeemin92@datasolution.kr>
2026-01-26 09:58:47 +0900

    Refactor: code prettier 수정

--- src/shared/supabase/index.ts
+++ src/shared/supabase/index.ts
@@ -1,4 +1,4 @@
 export { createSupabaseBrowserClient } from "./client";
 export { updateSession } from "./proxy";
-export { createSupabaseServerClient, hasSupabaseEnv } from "./server";
 export { shouldUseMockProjects } from "./runtime";
+export { createSupabaseServerClient, hasSupabaseEnv } from "./server";
